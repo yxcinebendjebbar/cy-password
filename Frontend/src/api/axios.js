4c5a22551bc88acd8209c8a085a3a43664a47c49
achraf <dodoben869@gmail.com>
2025-12-03 22:26:40 +0100

    adding axios.js file to api folder

---
+++ Frontend/src/api/axios.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+// 👇 التغيير هنا: نستخدم رابط جهازك المحلي
+const BASE_URL = "http://localhost:5000/api"; 
+
+const api = axios.create({
+    baseURL: BASE_URL
+});
+
+// هذا الكود يضيف التوكن تلقائياً لكل الطلبات (لا تغيره)
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('auth-token');
+    if (token) {
+        config.headers['auth-token'] = token;
+    }
+    return config;
+});
+
+export default api;
