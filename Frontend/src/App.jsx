f553c1158e0db032a21168e3fe6ee152b7fe7203
achraf <dodoben869@gmail.com>
2025-12-03 21:40:57 +0100

    Adding the frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Password Manager Frontend 🚀</h1>
+      <p>Working...</p>
+    </div>
+  );
+}
+
+export default App;
